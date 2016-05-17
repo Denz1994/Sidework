4e77ecebf36de6d04a9173019116a2085b779cff
Denzell <denz1994@gmail.com>
2016-05-17 11:10:08 -0400

    Solution now works as intended. (complete)

--- chess_board_sol.js
+++ chess_board_sol.js
@@ -1,12 +1,13 @@
 var firstLine=" #";
-var secLine="";
-var size = 11; 
+var secLine="# ";
+var size = 2;
 
 //----------------------------------------------------
 //generating the first line
 
 
 if (size%2==1){			// if size is odd
+  secLine=""
   	var lineSize=size-1;
 	for (var i=0;i<((size/2)-1);i++){
 		firstLine+=" #";
@@ -40,12 +41,21 @@ else					//if size is even
 
 //------------------------------------------------------
 
-/*console.log(firstLine)
-console.log(secLine);
-*/
 
 //Now the lines have been generated.
 //We can print these lines as many times as we want.
 
-if (size%2==0){
-    for (var j = 0; j <= size/2; j++)
+if (size%2==0)			//if the size is even
+    for (var j = 1; j <= size/2; j++){
+      	console.log(firstLine);
+  		console.log(secLine);
+    }
+
+else{					//if the size is odd
+    for (var j = 0; j <= (size/2)-1; j++){
+  		console.log(firstLine);
+  		console.log(secLine);
+    }
+	console.log(firstLine);
+}
+//This works as intended.
